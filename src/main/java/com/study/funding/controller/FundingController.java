9605d0aaa36f814c79ce5a285a23feacf88907ab
kimhyeonjeong <khjjung1124@gmail.com>
2024-07-10 14:14:59 +0900

    [feature-5] Todo 삭제

--- src/main/java/com/study/funding/controller/FundingController.java
+++ src/main/java/com/study/funding/controller/FundingController.java
@@ -21,7 +21,6 @@ public class FundingController {
     public FundingResponse productFunding(@RequestAttribute Long memberId,
                                           @PathVariable(name= "product_id") @Positive Long productId,
                                           @RequestBody @Valid FundingRequest fundingRequest) {
-        //TODO : @RequestBody FundingRequest 객체 데이터 타입이 다를 경우 예외처리
-        return fundingService.productFunding(memberId, productId, fundingRequest);
+          return fundingService.productFunding(memberId, productId, fundingRequest);
     }
 }
